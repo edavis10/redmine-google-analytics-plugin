4b40da9b9abc6735eee4a3808d59d90c80eb658a
Eric Davis <edavis@littlestreamsoftware.com>
2008-09-29 22:01:12 -0700

    Added a basic administration panel and default settings. #1645

--- init.rb
+++ init.rb
@@ -1,8 +1,19 @@
 require 'redmine'
 
+RAILS_DEFAULT_LOGGER.info 'Starting Google Analytics plugin for RedMine'
+
 Redmine::Plugin.register :google_analytics_plugin do
   name 'Google Analytics plugin'
   author 'Eric Davis'
   description 'Redmine plugin to insert the Google Analytics tracking code into Redmine based on user roles.'
   version '0.1.0'
+  
+  settings :default => {
+    'google_analytics_code' => '',
+    'google_analytics_log_anonymous' => true,
+    'google_analytics_log_authenticated' => true,
+    'google_analytics_log_administrator' => true
+  }, :partial => 'settings/google_analytics_settings'
+
+
 end
