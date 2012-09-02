be09e73fac73b8eff2aae6e46192de4020ffffca
Raphael Kallensee <raphael@kallensee.name>
2012-09-02 16:49:29 +0200

    Moved init.rb to the plugin root.

--- init.rb
+++ init.rb
@@ -1,1 +1,24 @@
-require File.dirname(__FILE__) + "/rails/init"
+require 'redmine'
+
+require_dependency 'google_analytics_hooks'
+
+Redmine::Plugin.register :google_analytics_plugin do
+  name 'Google Analytics plugin'
+  author 'Eric Davis'
+  description 'Redmine plugin to insert the Google Analytics tracking code into Redmine based on user roles.'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-analytics'
+  author_url 'http://www.littlestreamsoftware.com'
+
+  version '0.3.0-beta'
+
+  requires_redmine :version_or_higher => '2.0.0'
+
+  settings :default => {
+    'google_analytics_code' => '',
+    'google_analytics_log_anonymous' => true,
+    'google_analytics_log_authenticated' => true,
+    'google_analytics_log_administrator' => true
+  }, :partial => 'google_analytics_settings'
+
+end
+
